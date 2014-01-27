266ac2a8337fef708696d84442daf919f461e986
Chris Miller <cmille142@gmail.com>
2014-01-27 03:26:41 -0800

    added uglify step

---
+++ .grunt/uglify.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+  build: {
+    files: {
+      'public/js/main.build.min.js': ['public/js/main.build.js']
+    }
+  },
+
+  nprogress : {
+    files : {
+      'public/components/nprogress/nprogress.min.js' : ['public/components/nprogress/nprogress.js']
+    }
+  }
+
+};
--- Gruntfile.js
+++ Gruntfile.js
@@ -35,6 +35,7 @@ module.exports = function (grunt) {
 //    'jshint',
     'sass',
     'browserify',
+    'uglify',
     'connect',
     'watch'
   ]);
